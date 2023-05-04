2f68924e118a36719fa256c2492a84ce7ef4675a
Bob Belderbos <bobbelderbos@gmail.com>
2023-05-04 20:22:05 +0200

    using bulk insert

--- books/management/commands/populate_data.py
+++ books/management/commands/populate_data.py
@@ -1,5 +1,3 @@
-import random
-
 from django.core.management.base import BaseCommand
 
 from books.models import Author, Book
@@ -12,17 +10,19 @@ class Command(BaseCommand):
         # Create 1000 authors
         authors = []
         for i in range(1000):
-            author = Author.objects.create(name=f'Author {i}')
+            author = Author(name=f'Author {i}')
             authors.append(author)
+
+        Author.objects.bulk_create(authors)
 
         # Create 1000 books for each author
         books = []
         for author in authors:
             for i in range(1000):
-                book = Book.objects.create(
-                    title=f'Book {i}',
-                    author=author,
-                )
+                book = Book(title=f'Book {i}', author_id=author.id)
                 books.append(book)
 
+        Book.objects.bulk_create(books)
+
         self.stdout.write(self.style.SUCCESS(f'Successfully created {len(authors)} authors and {len(books)} books.'))
+
