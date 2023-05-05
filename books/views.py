65a13f7269508378493291fd43a66b260397463d
Bob Belderbos <bobbelderbos@gmail.com>
2023-05-05 15:39:31 +0200

    fix as per youtube video

--- books/views.py
+++ books/views.py
@@ -4,5 +4,5 @@ from books.models import Book
 
 
 def book_list(request):
-    books = Book.objects.all()[:1_000]
+    books = Book.objects.select_related("author")[:1_000]
     return render(request, 'books/book_list.html', {'books': books})
